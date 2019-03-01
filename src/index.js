a7f774894a7666151b588e71a4401b5d781df6c6
Rob <robbarton27@gmail.com>
2019-03-01 14:00:42 +0000

    Extended the file layout

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import "./style/style.scss";
-import "./js/tictactoe.jsx";
+import "./js/app.jsx";
 import "./img/images.png";
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './js/app.jsx';
+
+ReactDOM.render(<App />, document.getElementById('root'));
